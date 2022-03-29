4fabc4ad536081ff5dfa32a4307d7d9ea922ebcd
Christopher Coffee <chrisc1891@gmail.com>
2022-03-29 00:30:34 -0700

    Time: 446 ms (61.78%), Space: 46.3 MB (78.73%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.kt
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.kt
@@ -1,35 +1,20 @@
 class Solution {
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val map = mutableMapOf<Int,Int>()
         
-        val map = mutableMapOf<Int, Int>()
-        
-        //add the count of each number to the map
-        nums.forEach{ num ->
+        nums.forEach{ num -> 
             map[num] = map.getOrDefault(num, 0) + 1
         }
         
-        // create a custom comparator to use the count of the numbers as the priority
-        val minHeap = PriorityQueue<Int>( compareBy { map[it] } )
+        val priorityQueue = PriorityQueue<Int>( compareBy{ map[it] } )
         
-        //add the keys( each number ) to the heap based on the count in the map 
-        // if the minheap size is bigger than k, we want to remove the first element,
-        // because now there could be a new count and we want the top k
-        for( key in map.keys ){
-            minHeap.offer(key)
-            if(minHeap.size > k) minHeap.poll()
+        for(key in map.keys){
+            priorityQueue.offer(key)
+            if(priorityQueue.size > k) priorityQueue.poll()
         }
         
-        //create an array that is the size of k
         val topK = IntArray(k)
-        
-        //add the first k elements in the minHeap to the array
-        //(problem statement mentions order does not matter)
-        for( i in 0 until k ){
-          topK[i] = minHeap.poll()  
-        } 
-        
+        for(i in 0 until k) topK[i] = priorityQueue.poll()
         return topK
-        
-        
     }
 }
