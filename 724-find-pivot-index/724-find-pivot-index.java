0677f09a37ea077b97ef970a3b8168a4cecb7f5a
Christopher Coffee <chrisc1891@gmail.com>
2022-07-10 17:33:14 -0700

    Time: 3 ms (35.69%), Space: 52.2 MB (64.93%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        int totalSum = 0;
+        int leftSum = 0;
+        
+        for(int num: nums) totalSum += num;
+        
+        for(int i = 0;i < nums.length;i++){
+           if(totalSum - leftSum - nums[i] == leftSum) return i; 
+           leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+}
