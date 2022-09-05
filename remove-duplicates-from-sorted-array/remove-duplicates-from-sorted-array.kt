4133e19ff94f582cd6f5a06361c77a4f7d6dcd08
Christopher Coffee <chrisc1891@gmail.com>
2022-09-04 22:37:06 -0700

    Time: 310 ms, Memory: 49.2 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.kt
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.kt
@@ -1,14 +1,11 @@
 class Solution {
     fun removeDuplicates(nums: IntArray): Int {
-        var count = 0
+        var k = 0
         for(i in 1 until nums.size){
-            val left = nums[count]
-            val right = nums[i]
-            if(left != right){
-                nums[++count] = right
-            }
+           if(nums[i] != nums[i-1]){
+               nums[++k] = nums[i]
+           } 
         }
-        
-        return count + 1
+        return k+1
     }
 }
