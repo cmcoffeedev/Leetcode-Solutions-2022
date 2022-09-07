fc8a5c8cf74b65956923ae39df796191beca19f9
Christopher Coffee <chrisc1891@gmail.com>
2022-09-06 20:06:26 -0700

    Time: 293 ms (27.76%), Space: 35.3 MB (60.48%) - LeetHub

--- 27-remove-element/27-remove-element.kt
+++ 27-remove-element/27-remove-element.kt
@@ -1,51 +1,11 @@
 class Solution {
     fun removeElement(nums: IntArray, `val`: Int): Int {
-        /*
-        nums = [3,2,2,3], val = 3
-        nums= [2,2], val = 3
-        */
-           //val prev = nums[i-1]
-       var validNums = 0 
-        val lastIndex = nums.size-1
-         var lastValidIndex = lastIndex //3, //3
-       for(currentIndex in 0 until nums.size){
-           val current = nums[currentIndex] //3, //2, //3
-           if(current == `val`){
-               
-              
-               while(lastValidIndex >= 0 && nums[lastValidIndex] ==  `val`){
-                    lastValidIndex-- //2,
-                   //2, //1
-               }
-               
-               if(lastValidIndex <= currentIndex) break
-               
-               
-               val last = nums[lastValidIndex] //nums[2] = 2
-               //swap current index with lastValidIndex's num
-               nums[currentIndex] = last //nums[0] = 2
-               //swap lastValidIndex with the current num
-               nums[lastValidIndex] = current //nums[2] = 2
-               
-               //2,2,3,3
-               //validNums = 1
-               
-               //initial = [0,1,2,2,3,0,4,2], val = 2
-               //[0,1,2,2,3,0,4,5], val = 2
-               //[0,1,5,2,3,0,4,2], val = 2
-               //[0,1,5,4,3,0,2,2], val = 2
-               //     i
-               //[0,1,2,3,0,4,2,2], val = 2
-               //     i
-               //[0,1,3,0,4,2,2,2], val = 2
-              
-           }
-           
-           validNums++ 
-               //validNums = 1
-               //validNums = 2
-       }
-       
-       return validNums
+        var validSize = 0
+        for(num in nums){
+            if(num != `val`){
+                nums[validSize++] = num
+            }
+        }
+        return validSize
     }
 }
