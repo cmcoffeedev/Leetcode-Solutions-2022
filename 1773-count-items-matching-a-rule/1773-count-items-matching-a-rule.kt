292bde9aed05f575fffe0874118ed5a8eb7379a1
Christopher Coffee <chrisc1891@gmail.com>
2022-09-07 12:08:31 -0700

    Time: 532 ms (34.61%), Space: 62.2 MB (88.46%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.kt
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.kt
@@ -1,7 +1,12 @@
 class Solution {
     fun countMatches(items: List<List<String>>, ruleKey: String, ruleValue: String): Int {
         
-        val map = mutableMapOf("type" to 0, "color" to 1, "name" to 2)
+        val map = mutableMapOf(
+            "type" to 0,
+            "color" to 1,
+            "name" to 2
+        )
+        
         val indexToMatch = map[ruleKey]
         
         var matches = 0
