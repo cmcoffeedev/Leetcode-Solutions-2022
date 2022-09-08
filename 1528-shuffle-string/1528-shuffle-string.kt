ab9075032d41ba106b9a3e39bd14b5425d88ae25
Christopher Coffee <chrisc1891@gmail.com>
2022-09-08 02:58:49 -0700

    Time: 404 ms (22.58%), Space: 38.4 MB (96.77%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.kt
+++ 1528-shuffle-string/1528-shuffle-string.kt
@@ -1,19 +1,14 @@
 class Solution {
     fun restoreString(s: String, indices: IntArray): String {
         var sArray = s.toCharArray()
-        val map = mutableMapOf<Int, Char>()
-        val sBuilder = StringBuilder()
+        val cArray = CharArray(sArray.size)
         
         for(i in 0 until indices.size){
             val currentChar = sArray[i]
             val currentIndex = indices[i]
-            map[currentIndex] = currentChar
+            cArray[currentIndex] = currentChar 
         }
         
-        for(i in 0 until indices.size){
-           sBuilder.append( map[i] )
-        }
-        
-        return sBuilder.toString()
+        return String(cArray)
     }
 }
