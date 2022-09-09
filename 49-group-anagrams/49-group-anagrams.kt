dcc601a3aedeffc3baa1ea88eeb29dc5faaeb361
Christopher Coffee <chrisc1891@gmail.com>
2022-09-09 14:33:21 -0700

    Time: 996 ms (15.61%), Space: 76 MB (45.22%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.kt
+++ 49-group-anagrams/49-group-anagrams.kt
@@ -2,7 +2,6 @@ class Solution {
     fun groupAnagrams(strs: Array<String>): List<List<String>> {
         
         val map = mutableMapOf<String, ArrayList<String>>()
-        val list = ArrayList<List<String>>()
         
         for(word in strs){
             val sortedWord = word.split("").sorted().joinToString("")
@@ -11,11 +10,6 @@ class Solution {
             map[sortedWord] = groupList
         }
         
-        map.forEach{ entry ->
-            val groupList = entry.value
-            list.add(groupList)
-        }
-        
-        return list
+        return map.values.toList()
     }
 }
