3e02e18a04f9c1d8209f55daf0d11dc317bc6b4c
Christopher Coffee <chrisc1891@gmail.com>
2022-09-13 19:45:29 -0700

    Time: 250 ms (29.03%), Space: 34.8 MB (67.74%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun countAsterisks(s: String): Int {
+        //create a charArray and loop through it
+        // var numOfBars = 0
+        // var asterickCount
+        //once I get to a vertical bar count it,
+        // count all asterisks when numOfBars == 1
+        // once I get to 2nd bar, set numOfBars = 0 and 
+        // ignore characters until I see another bar
+        
+         var numOfBars = 0
+         var asterickCount = 0
+         val sArray = s.toLowerCase().toCharArray()
+         for(character in sArray){
+             
+            //if(character >= 'a' && character <= 'Z') 
+             if(character == '*' && numOfBars == 0){
+                 asterickCount++
+             }
+             else if(character == '|' && numOfBars == 1){
+                 numOfBars = 0 
+             }
+             else if(character == '|' && numOfBars == 0){
+                 numOfBars = 1 
+             }
+         }
+         // "l|*e*et|c**o|*de|"
+         
+         return asterickCount
+         
+    }
+}
