3439a9b3289406e56fbaa9c36136e07d58627e2e
Christopher Coffee <chrisc1891@gmail.com>
2022-09-17 02:13:30 -0700

    Time: 247 ms (32.95%), Space: 34.5 MB (12.98%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.kt
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.kt
@@ -9,9 +9,8 @@
  */
 class Solution {
     fun middleNode(head: ListNode?): ListNode? {
-        var tmp = head
-        var slow = tmp
-        var fast = tmp
+        var slow = head
+        var fast = head
         while(fast != null && fast?.next != null){
             slow = slow?.next
             fast = fast?.next?.next
