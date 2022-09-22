798d69048148e38299a7fbc584772d9b181ca212
Christopher Coffee <chrisc1891@gmail.com>
2022-09-22 01:41:45 -0700

    Time: 491 ms (8.13%), Space: 33.9 MB (13.15%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.kt
+++ 278-first-bad-version/278-first-bad-version.kt
@@ -3,15 +3,23 @@
 
 class Solution: VersionControl() {
     override fun firstBadVersion(n: Int) : Int {
-        var left = 1
+        
+        var left = 0
         var right = n
-        while(left < right){
+        
+        while(left <= right){
             val mid = left + (right - left)/2
+            
             if(isBadVersion(mid)){
-                right = mid
+                right = mid - 1
             }
-            else left = mid + 1
+            else if(!isBadVersion(mid)){
+                left = mid + 1
+            }
+            
         }
+        
         return left
+        
 	}
 }
