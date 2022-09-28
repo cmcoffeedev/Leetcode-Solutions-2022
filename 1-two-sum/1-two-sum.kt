f793a143a79b5df6784a061dad6e48ffaef9ac3b
Christopher Coffee <chrisc1891@gmail.com>
2022-09-27 19:26:14 -0700

    Time: 384 ms (63.26%), Space: 41.5 MB (63.26%) - LeetHub

--- 1-two-sum/1-two-sum.kt
+++ 1-two-sum/1-two-sum.kt
@@ -1,14 +1,18 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        // a map to hold the number and it's index in the array
-        val map = HashMap<Int, Int>() 
-        
-        nums.forEachIndexed{ i, currentNum ->
-            val complement = target - currentNum
-            map[complement]?.let{ return intArrayOf(it, i) }
-            map.put(currentNum, i)
+        val map = mutableMapOf<Int, Int>()
+        nums.forEachIndexed{ index, num ->
+            val difference = target - num
+            map[difference]?.let{ diffIndex -> 
+                return intArrayOf(diffIndex, index)
+            }
+            map[num] = index
         }
         
-        return intArrayOf(-1,-1)
+        return intArrayOf(-1, -1)
+        
+        //[2,7,11,15]
+     //   [3,2,4],
+        
     }
 }
